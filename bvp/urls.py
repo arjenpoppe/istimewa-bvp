1eab350fc802e13c40dec4ab6a674a0358df1980
Arjen Poppe <arjen_poppe@live.nl>
2020-03-04 11:55:50 +0100

    add dashboard and pi_generator modules

--- bvp/urls.py
+++ bvp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('pi_generator/', include('pi_generator.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ pi_generator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PiGeneratorConfig(AppConfig):
+    name = 'pi_generator'
