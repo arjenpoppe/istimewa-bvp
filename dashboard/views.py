302b423d9d2fb79cd7e2850a76c886e4a6785876
Arjen Poppe <arjen_poppe@live.nl>
2020-03-06 15:49:57 +0100

    add bootstrap template to dashboard app

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Generatre dashboard here!")
+    return render(request, 'dashboard/index.html')
