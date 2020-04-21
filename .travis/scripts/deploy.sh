8f94dc46b05b4ed32e512f4d5b6eedef5f4c0340
Arjen Poppe <arjen_poppe@live.nl>
2020-04-21 14:11:49 +0200

    deployment test #11

--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -9,6 +9,8 @@ git config --global push.default matching
 git remote add deploy ssh://administrator@$IP:$PORT$DEPLOY_DIR
 git push deploy release
 
+echo "$IP:$PORT$DEPLOY_DIR"
+
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh administrator@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
