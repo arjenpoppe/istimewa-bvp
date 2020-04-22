3c4901752f6b20efcb93432720449b2c99bceca5
Arjen Poppe <arjen_poppe@live.nl>
2020-04-22 08:51:28 +0200

    latest update to travis.yml

--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -5,9 +5,9 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-# git config --global push.default matching
-# git remote add deploy ssh://administrator@$IP:$PORT$DEPLOY_DIR
-# git push deploy release
+git config --global push.default matching
+git remote add deploy ssh://administrator@$IP:$PORT$DEPLOY_DIR
+git push deploy release
 
 # echo "$IP:$PORT$DEPLOY_DIR"
 
