33aef339aff3c825dd6427f0afaaa5263db71ca1
Arjen Poppe <arjen_poppe@live.nl>
2020-04-22 23:08:27 +0200

    add vpi search functionality

--- vpi/static/vpi/js/main.js
+++ vpi/static/vpi/js/main.js
@@ -9,11 +9,11 @@ let ajax_call = function (endpoint, request_parameters) {
     $.getJSON(endpoint, request_parameters)
         .done(response => {
             // fade out the artists_div, then:
-            vpis_div.fadeTo('slow', 0).promise().then(() => {
+            vpis_div.fadeTo('fast', 0).promise().then(() => {
                 // replace the HTML contents
                 vpis_div.html(response['html_from_view'])
                 // fade-in the div with new contents
-                vpis_div.fadeTo('slow', 1)
+                vpis_div.fadeTo('fast', 1)
                 // stop animating search icon
                 search_icon.removeClass('blink')
             })
