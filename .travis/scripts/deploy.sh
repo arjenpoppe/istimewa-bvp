aa9debbb8a9178e2d2d1d04fd3b0ef4bb7d4a9e3
Arjen Poppe <arjen_poppe@live.nl>
2020-04-25 13:58:59 +0200

    disable deployment

--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "deploy script called"
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
-
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh -tt administrator@$IP -p $PORT <<EOF
-  cd documents/istimewa-bvp
-  git pull origin release
-  python -m pip install -r requirements.txt
-  python manage.py migrate
-  python manage.py collectstatic --noinput
-EOF
+#eval "$(ssh-agent -s)" # Start ssh-agent cache
+#chmod 600 .travis/id_rsa # Allow read access to the private key
+#ssh-add .travis/id_rsa # Add the private key to SSH
+#
+## Skip this command if you don't need to execute any additional commands after deploying.
+#ssh -tt administrator@$IP -p $PORT <<EOF
+#  cd documents/istimewa-bvp
+#  git pull origin release
+#  python -m pip install -r requirements.txt
+#  python manage.py migrate
+#  python manage.py collectstatic --noinput
+#EOF
 
 echo "deployment successful"
 
