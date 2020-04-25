b1416c8bf4c5f02f18eb636fdc70951637bcaec1
Arjen Poppe <arjen_poppe@live.nl>
2020-04-25 13:05:08 +0200

    deployment test #15

--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 echo "deploy script called"
 
-#eval "$(ssh-agent -s)" # Start ssh-agent cache
-#chmod 600 .travis/id_rsa # Allow read access to the private key
-#ssh-add .travis/id_rsa # Add the private key to SSH
-#
-#git config --global push.default matching
-#git remote add deploy ssh://administrator@$IP:$PORT$DEPLOY_DIR
-#git push deploy release
-#
-## echo "$IP:$PORT$DEPLOY_DIR"
-#
-## Skip this command if you don't need to execute any additional commands after deploying.
-#ssh administrator@$IP -p $PORT <<EOF
-#  cd $DEPLOY_DIR
-#  python -m pip install -r requirements.txt
-#  python manage.py makemigrations
-#  python manage.py migrate
-#  python manage.py collectstatic
-#  powershell Restart-Service Apache2.4
-#EOF
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh administrator@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  git pull origin release
+  python -m pip install -r requirements.txt
+  python manage.py migrate
+  python manage.py collectstatic
+  powershell Restart-Service Apache2.4
+EOF
 
 echo "deployment successful"
